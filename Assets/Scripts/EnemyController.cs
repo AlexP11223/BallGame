b10a4e5e5f0ea5e17b4025bfb96c668d5a8c8c37
Alex P <alex.pantec@gmail.com>
2018-03-12 20:15:43 +0200

    evil ball prefab

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator _anim;
+    private bool _isDead = false;
+
+    public AudioSource SplatSound;
+    public AudioSource SquelchSound;
+
+    public bool IsDead
+    {
+        get { return _isDead; }
+        private set
+        {
+            _isDead = value;
+            _anim.SetBool(nameof(IsDead), IsDead);
+        }
+    }
+    
+    void Start ()
+    {
+        _anim = GetComponentInParent<Animator>();
+    }
+	
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (IsDead)
+        {
+            SquelchSound.Play();
+        }
+        else
+        {
+            SplatSound.Play();
+        }
+
+        IsDead = true;
+
+    }
+}
