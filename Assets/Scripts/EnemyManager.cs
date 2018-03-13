585e6b0eb4cc798a23a99ce253995c4be6216f78
Alex P <alex.pantec@gmail.com>
2018-03-13 20:33:49 +0200

    trampoline enemies

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -16,8 +16,7 @@ public class EnemyManager : MonoBehaviour
         {
             Spawn();
         }
-
-        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        
         InvokeRepeating(nameof(Spawn), SpawnDelay, SpawnDelay);
     }
 
---
+++ Assets/Scripts/FallDetector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class FallDetector : MonoBehaviour
+{
+    private bool _isFallDetected = false;
+
+    public SpawnArea TrampolineSpawnArea;
+
+    public int EnemiesCount = 1000;
+
+    public GameObject EnemyToSpawn;
+
+    public float SpawnDelay = 2f;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!_isFallDetected && collider.CompareTag("Player"))
+        {
+            _isFallDetected = true;
+
+            for (int i = 0; i < EnemiesCount; i++)
+            {
+                TrampolineSpawnArea.Spawn(EnemyToSpawn);
+            }
+
+            InvokeRepeating(nameof(Spawn), SpawnDelay, SpawnDelay);
+        }
+    }
+
+    void Spawn()
+    {
+        TrampolineSpawnArea.Spawn(EnemyToSpawn);
+    }
+}
