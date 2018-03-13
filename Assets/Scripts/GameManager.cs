d75c26584334ebc950f83788bf5d18527e9279e3
Alex P <alex.pantec@gmail.com>
2018-03-13 23:53:41 +0200

    increase player size on each kill

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,10 @@ public class GameManager : MonoBehaviour
 
     public Text ScoreText;
 
+    public GameObject Player;
+
+    public float PlayerSizeIncrement = 0.05f;
+
 	void Update()
 	{
 	    ScoreText.text = $"Score: {_score}";
@@ -15,5 +19,7 @@ public class GameManager : MonoBehaviour
     public void HandleKill()
     {
         _score++;
+
+        Player.transform.localScale += new Vector3(PlayerSizeIncrement, PlayerSizeIncrement, PlayerSizeIncrement);
     }
 }
