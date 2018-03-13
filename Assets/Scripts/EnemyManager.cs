f199248286d03ba895fcb1668dd0182c860fad17
Alex P <alex.pantec@gmail.com>
2018-03-13 15:44:38 +0200

    spawn areas

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject EnemyToSpawn;
+
+    public float SpawnDelay = 2f;
+    
+    public SpawnArea[] SpawnAreas;
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating(nameof(Spawn), SpawnDelay, SpawnDelay);
+    }
+
+    void Spawn()
+    {
+        var spawnArea = SpawnAreas[Random.Range(0, SpawnAreas.Length)];
+
+        spawnArea.Spawn(EnemyToSpawn);
+    }
+}
---
+++ Assets/Scripts/SpawnArea.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArea : MonoBehaviour
+{
+    public void Spawn(GameObject enemyToSpawn)
+    {
+        var pos = new Vector3(Random.Range(transform.position.x - transform.localScale.x / 2, transform.position.x + transform.localScale.x / 2),
+            transform.position.y,
+            Random.Range(transform.position.z - transform.localScale.z / 2, transform.position.z + transform.localScale.z / 2));
+
+        Instantiate(enemyToSpawn, pos, transform.rotation);
+    }
+}
