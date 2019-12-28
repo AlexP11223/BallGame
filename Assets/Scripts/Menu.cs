930023ce5ab40e147f9068af75064a097d63e6cb
Alex P <alex.pantec@gmail.com>
2019-12-28 19:48:29 +0200

    adjust menu in webgl

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,6 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    void Start()
+    {
+        // handle it here to avoid unnecessary regeneration of the scene in newer Unity version
+        bool canQuit = Application.platform != RuntimePlatform.WebGLPlayer;
+        if (!canQuit)
+        {
+            if (gameObject.name == "QuitButton")
+            {
+                gameObject.SetActive(false);
+            }
+            if (gameObject.name == "RestartButton")
+            {
+                var btn = gameObject.GetComponent<RectTransform>();
+                btn.anchoredPosition = new Vector2(0.0f, btn.anchoredPosition.y);
+            }
+        }
+    }
+
     public void OnQuit()
     {
         Application.Quit();
